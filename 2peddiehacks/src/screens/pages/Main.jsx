7f57521979e3d3e32c6735386664f776c1d90876
Anita Yip <ceruleanox@gmail.com>
2022-08-20 16:35:08 -0400

    separated Navbar component

--- 2peddiehacks/src/screens/pages/Main.jsx
+++ 2peddiehacks/src/screens/pages/Main.jsx
@@ -1,6 +1,7 @@
 import logo from '../../static/logo.svg';
 import text from '../../static/placeholder.gif';
 import {Link} from 'react-router-dom';
+import Navbar from '../../components/Navbar'
 
 
 
@@ -17,75 +18,40 @@ export default function HomePage() {
   //   console.log(event.currentTarget);
   // };
     return(
+      <>
+        <Navbar />
         <body>
-        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-  <div class="container flex flex-wrap justify-between items-center mx-auto">
-    <a href="#!" class="flex items-center">
-        <img src={logo} class="mr-3 h-10 sm:h-20" alt="Flowbite Logo" />
-        <span class="self-center text-5xl	font-semibold whitespace-nowrap dark:text-white">Flows</span>
-    </a>
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-      <span class="sr-only">Open main menu</span>
-      <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-    </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <li>
-          <a href="#!" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</a>
-        </li>
-        <li>
-          <a href="#!" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-        </li>
-        <li>
-          <a href="#!" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-        </li>
-        <li>
-          <a href="#!" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-        </li>
-        <li>
-          <a href="#!" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<body>
-<div class="h-screen flex">
-<div class="mt-2 flex w-1/2 justify-center items-center bg-white">
+          <div class="h-screen flex">
+          <div class="mt-2 flex w-1/2 justify-center items-center bg-white">
+            <img class="pb-21"src={text} alt="streamline with us"></img>
+          </div>
+          <div class="flex w-1/2 justify-center items-center bg-white">
+            <form class="bg-white">
+                <h1 class="font-semibold whitespace-nowrap dark:text-white text-2xl mb-1">Hey there!</h1>
+                <p class="text-sm font-normal text-gray-600 mb-7">Join our mission to empower students!</p>
+                <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                  </svg>
+                  <input class="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Email Address" />
+                </div>
+                <div class="flex items-center border-2 py-2 px-3 rounded-2xl">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                  </svg>
+                  <input class="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Password" />
+                </div>
+                <div>
+                <Link to="/main">
+                <button type="submit" class="block w-full bg-blue-700 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Login</button></Link>
+                <span class="pl-12 text-sm ml-0 hover:text-blue-500 cursor-pointer">Don't have an account? Sign up</span>
+                </div>
+              </form>
 
-    <img class="pb-21"src={text} alt="streamline with us"></img>
-  </div>
+            </div>
+            </div>
 
-  <div class="flex w-1/2 justify-center items-center bg-white">
-  <form class="bg-white">
-      <h1 class="font-semibold whitespace-nowrap dark:text-white text-2xl mb-1">Hey there!</h1>
-      <p class="text-sm font-normal text-gray-600 mb-7">Join our mission to empower students!</p>
-      <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-        </svg>
-        <input class="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Email Address" />
-      </div>
-      <div class="flex items-center border-2 py-2 px-3 rounded-2xl">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
-        </svg>
-        <input class="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Password" />
-      </div>
-      <div>
-      <Link to="/main">
-      <button type="submit" class="block w-full bg-blue-700 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Login</button></Link>
-      <span class="pl-12 text-sm ml-0 hover:text-blue-500 cursor-pointer">Don't have an account? Sign up</span>
-      </div>
-    </form>
-
-  </div>
-  </div>
-
-    
-      </body>
-
-      </body>
-    
+        </body>
+      </>
     )
 }
